6599b7d222c5000a37d77e1595d6b6e93142c393
gregory_clarke <g.g.clarke@gmail.com>
2018-03-24 16:06:50 +0000

    fixed integrationTest build

---
+++ setEnv.bat
@@ -0,0 +1,7 @@
+
+set FV_CLASSPATH=C:/source/filevinder/eng/build/libs/
+set FV_INDEX_FILE_EXT=.ffdr
+set FV_ENG_JAR=filevinder-eng-1.0.0-SNAPSHOT.jar
+set FV_SEARCH_CLASS=org.filevinder.core.SearchImpl
+set FV_SEARCH_CACHE_FILE=c:/temp/fv_cache.txt
+set FV_TEST_ROOT=c:/source/filevinder/testfiles/
